aac41994e53ebff19ed5f846b3442e6564559c7e
Royce Chua <royce236@gmail.com>
2022-07-06 00:43:11 +0800

    Add typescript

--- index.js
+++ index.js
@@ -1,88 +1,77 @@
-const express = require("express");
-const app = express();
-
-/* 
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var express = require("express");
+var app = express();
+/*
 middleware
 request -> node server -> middleware -> function()
 */
 // Body parsing middleware
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-// EVENT REGISTRATION API
-
-let events = [
-    {id:"1", title: "event 1", date: "july 10, 2022", loc: "qc", price: 100},
-    {id:"2", title: "event 2", date: "july 12, 2022", loc: "qc", price: 0},
-    {id:"3", title: "event 3", date: "august 23, 2022", loc: "makati", price: 250},
-    {id:"4", title: "event 4", date: "october 27, 2022", loc: "pasig", price: 300},
-    {id:"5", title: "event 5", date: "december 30, 2022", loc: "marikina", price: 500},
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+var events = [
+    { id: "1", title: "event 1", date: "july 10, 2022", loc: "qc", price: 100 },
+    { id: "2", title: "event 2", date: "july 12, 2022", loc: "qc", price: 0 },
+    { id: "3", title: "event 3", date: "august 23, 2022", loc: "makati", price: 250 },
+    { id: "4", title: "event 4", date: "october 27, 2022", loc: "pasig", price: 300 },
+    { id: "5", title: "event 5", date: "december 30, 2022", loc: "marikina", price: 500 },
 ];
-// SELECT * FROM events WHERE price>100
-
-app.get("/", (req, res) => {
-    res.send("Event Registration API");
+app.get("/", function (req, res) {
+    return res.send("Event Registration API");
 });
-
 // possible nouns: users, events, registrations, categories
-app.get("/events", (req, res) => {
-    const query = req.query;
-    
-    if(Object.keys(query).length === 0) { // if no query params
-        res.status(200).send({message: "Request successful", data: events});
+app.get("/events", function (req, res) {
+    var query = req.query;
+    if (Object.keys(query).length === 0) { // if no query params
+        return res.status(200).send({ message: "Request successful", data: events });
     }
-
     // if query params were given
-    let filtered_events = []
+    var filtered_events = [];
     // database operation
-    if(query.title) {
-        filtered_events = events.filter(event => query.title.toLowerCase() === event.title);
+    if (query.title) {
+        filtered_events = events.filter(function (event) { return query.title.toLowerCase() === event.title; });
     }
-
-    if(query.loc) {
-        filtered_events = events.filter(event => query.loc.toLowerCase() === event.loc);
+    if (query.loc) {
+        filtered_events = events.filter(function (event) { return query.loc.toLowerCase() === event.loc; });
     }
-
-    if(filtered_events.length > 0) {
-        res.status(200).send({message: "Request successful", data: filtered_events});
-    } 
-
-    res.status(200).send({message: "Request successful", data: []});
+    if (filtered_events.length > 0) {
+        return res.status(200).send({ message: "Request successful", data: filtered_events });
+    }
+    return res.status(200).send({ message: "Request successful", data: [] });
 });
-
-app.get("/events/:id", (req, res) => {
-    const params = req.params;
-    const id = params.id;
+app.get("/events/:id", function (req, res) {
+    var params = req.params;
+    var id = params.id;
     // database operation to query specific record
-    const event = events.find((event) => id === event.id); // undefined if nothing found
-    if(!event) {
-        res.status(200).send({message: "Request successful", data: []});
+    var event = events.find(function (event) { return id === event.id; }); // undefined if nothing found
+    if (!event) {
+        return res.status(200).send({ message: "Request successful", data: [] });
     }
-    res.status(200).send({message: "Request successful", data: event});
+    return res.status(200).send({ message: "Request successful", data: event });
 });
-
-app.post("/events", (req, res) => {
-    const body = req.body;
-    if(!body.title) {
-        res.status(422).send({message: "Missing title"});
+app.post("/events", function (req, res) {
+    var body = req.body;
+    if (!body.title) {
+        res.status(422).send({ message: "Missing title" });
     }
     // database operation to insert new record to db
-    events = [...events, body];
-    res.status(201).send({message: "Event successfully added", data: body});
+    events = __spreadArray(__spreadArray([], events, true), [body], false);
+    return res.status(201).send({ message: "Event successfully added", data: body });
 });
-
-app.put("/events", (req, res) => {
-    const body = req.body;
-    const dataWithoutBody = events.filter((event) => body.id !== event.id); 
-    events = [...dataWithoutBody, body];
-    res.status(200).send({message: "Event successfully updated"});
+app.put("/events", function (req, res) {
+    var body = req.body;
+    var dataWithoutBody = events.filter(function (event) { return body.id !== event.id; });
+    events = __spreadArray(__spreadArray([], dataWithoutBody, true), [body], false);
+    return res.status(200).send({ message: "Event successfully updated" });
 });
-
-app.delete("/events/:id", (req, res) => {
-    const params = req.params;
-    const id = params.id;
-    events = events.filter((event) => id !== event.id); 
-    res.status(200).send({message: "Event successfully deleted"});
-})
-
-app.listen(3000)
+app["delete"]("/events/:id", function (req, res) {
+    events;
+});
+app.listen(3000);
